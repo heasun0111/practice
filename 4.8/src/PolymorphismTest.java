e151eff003dc566be73af200ddd50d352f744170
heasun0111 <48648940+heasun0111@users.noreply.github.com>
2019-06-07 17:25:31 +0900

    Add files via upload

---
+++ 4.8/src/PolymorphismTest.java
@@ -0,0 +1,31 @@
+
+public class PolymorphismTest
+{
+	   public static void main(String[] args)
+	   {
+	      CommissionEmployee commissionEmployee = new CommissionEmployee(
+	            "Sue", "Jones", "222-22-2222", 10000, .06);
+	      
+	      BasePlusCommissionEmployee basePlusCommissionEmployee =
+	            new BasePlusCommissionEmployee(
+	                  "Bob", "Lewis", "333-33-3333", 5000, .04, 300);
+	      System.out.printf("%s %s:\n\n%s\n\n",
+	            "Call CommisionEmployee's toString with superclass reference",
+	            "to superclass object", commissionEmployee.toString() );
+	      System.out.printf("%s %s:\n\n%s\n\n",
+	            "Call BasePlusCommissionEmployee's toString with subclass",
+	            "reference to subclass object",
+	            basePlusCommissionEmployee.toString() );
+	      CommissionEmployee commissionEmployee2 =
+	            basePlusCommissionEmployee;
+	      System.out.printf("%s %s:\n\n%s\n\n",
+	            "Call BasePlusCommissionEmployee's toString with superclass",
+	            "reference to subclass object", commissionEmployee2.toString() );
+	      
+	      //디버그를 이용하면 그 코드에 대한 분석을 할 수 있다.
+	   
+	   }
+
+
+
+}
